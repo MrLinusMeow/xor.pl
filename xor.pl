b2b4d29a4cb8b7bd0c2893bed16b16a7d7aef281
Meow <mrlinusmeow@skiff.com>
2024-09-10 17:48:53 +0200

    improved;

--- xor.pl
+++ xor.pl
@@ -1,12 +1,28 @@
 #!/usr/bin/env perl
+
 unless ($ARGV[0]){
 	print "\e[107;31;1;5;7m___NULL___\e[0m\n";
 	exit;
 }
-
-my $a;
-foreach (@ARGV) {
-	$a = $a ^ $_;
+unless ($ARGV[1]){
+	print "@ARGV\n";
+	exit;
 }
-print "$a\n";
+my @answer;
+my $i = 0;
+my $j = 0;
+until (scalar @ARGV == 1){
+	my @next = split //, @ARGV[1];
+	foreach (split //, @ARGV[0]){
+		if(scalar @next == $j){ $j = 0 }
+		@answer[$i] = $_ ^ @next[$j];
+		++$j;
+		++$i;
+	}
+	$j = 0;
+	$i = 0;
+	shift @ARGV;
+	@ARGV[0] = join '', @answer;
+}
+print "\e[1;102;30m", join '',@answer ,"\e[0m\n";
 exit;
